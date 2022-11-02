d0fae3dbb303a248481b985291497d107c0cc814
Ramachary2222 <ramachary9110@gmail.com>
2022-11-02 19:55:27 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import './App.css';
-import 'rsuite/dist/rsuite.min.css';
+// eslint-disable-next-line import/no-unresolved
+import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './Components/PrivateRoute';
+import PublicRoute from './Components/PublicRoute';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
 import './styles/main.scss'
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRoute>
+        <Home />
+      </PrivateRoute>
+    </switch>
   );
 }
 
